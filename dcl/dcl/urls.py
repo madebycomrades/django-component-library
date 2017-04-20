0e90e38b60c010ed106960fc5c5d48ec32376d63
Matt Collins <mailmattcollins@gmail.com>
2017-04-20 17:25:56 +0100

    Basic example view to show components being used.

--- dcl/dcl/urls.py
+++ dcl/dcl/urls.py
@@ -2,7 +2,11 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
 
+from .views import HomeView
+
 urlpatterns = [
+
+    url(r'^$', HomeView.as_view(), name='home'),
     url(r'^admin/', admin.site.urls),
 
     url(r'^components/', include('components.urls', namespace='components')),
